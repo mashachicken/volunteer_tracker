5140913784c63445d562f4f73ef6a31cb788ec8f
Masha Ivy <mashanechmo@gmail.com>
2020-01-10 15:59:48 -0800

    finxed a syntax error that resolved in a  scoping issue that I couldnt find for 2 hours

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -29,7 +29,7 @@ class Volunteer
     DB.exec("DELETE FROM volunteers *;")
   end
   def self.find(id)
-    volunteer= DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
+    volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
     if volunteer
       name = volunteer.fetch("name")
       project_id = volunteer.fetch("project_id").to_i
@@ -38,12 +38,30 @@ class Volunteer
     else
       nil
     end
-    def update(name, project_id)
-      @name = name
-      @project_id = project_id
-      DB.exec("UPDATE volunteers SET name = '#{@name}', project_id = #{@project_id} WHERE id = #{@id};")
-    end
-    def delete
-      DB.exec("DELETE FROM citties WHERE id = #{@id};")
-    end
   end
+  def update(name, project_id)
+    @name = name
+    @project_id = project_id
+    DB.exec("UPDATE volunteers SET name = '#{@name}', project_id = #{@project_id} WHERE id = #{@id};")
+  end
+  def delete
+    DB.exec("DELETE FROM volunteers WHERE id = #{@id};")
+  end
+  def self.clear
+    DB.exec("DELETE FROM volunteers *;")
+  end
+  def self.find_by_project(proj_id)
+    volunteers = []
+    returned_volunteers = DB.exec("SELECT * FROM volunteers WHERE project_id = #{proj_id};")
+    binding.pry
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :project_id => proj_id, :id => id}))
+    end
+    volunteers
+  end
+  def project
+    Volunteer.find(@project_id)
+  end
+end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -42,23 +42,14 @@ describe Volunteer do
     end
   end
 
+  describe('.clear') do
+    it "clears all volunteers" do
+      volunteer = Volunteer.new({:name => 'Jane', :project_id => 1, :id => nil})
+      volunteer.save()
+      volunteer2 = Volunteer.new({:name => 'Joe', :project_id => 1, :id => nil})
+      volunteer2.save()
+      Volunteer.clear
+      expect(Volunteer.all).to(eq([]))
+    end
+  end
 end
-# describe('.clear') do
-#   it("clears all volunteers") do
-#     volunteer = Volunteer.new({:name => "Masha", :id => nil})
-#     volunteer.save()
-#     volunteer2 = Volunteer.new({:name => "Another Volunteer", :id => nil})
-#     volunteer2.save()
-#     Volunteer.clear
-#     expect(Volunteer.all).to(eq([]))
-#   end
-# end
-# describe('.find') do
-#   it("finds a volunreer by id") do
-#     volunreer = Volunteer.new({:name => "Maha", :id => nil})
-#     volunreer.save()
-#     volunreer2 = Volunteer.new({:name => "Another Volunteer", :id => nil})
-#     volunreer2.save()
-#     expect(Volunteer.find(volunreer.id)).to(eq(volunreer))
-#   end
-# end
