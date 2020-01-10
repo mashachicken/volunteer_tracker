f5ee1f806639f719de5ee111b373dbe7a12ccd4c
Masha Ivy <mashanechmo@gmail.com>
2020-01-10 11:23:55 -0800

    write more methods and pass tests for them

--- lib/volunteer.rb
+++ lib/volunteer.rb
@@ -6,13 +6,29 @@ class Volunteer
     @id = attributes.fetch(:id)
   end
   def self.all
-  returned_volunteers = DB.exec("SELECT * FROM volunteers;")
-  volunteers = []
-  returned_volunteers.each() do |volunteer|
-    name = volunteer.fetch("name")
-    id = volunteer.fetch("id").to_i
-    volunteers.push(Volunteer.new({:name => name, :id => id}))
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      name = volunteer.fetch("name")
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:name => name, :id => id}))
+    end
+    volunteers
   end
-  volunteers
+  def ==(other)
+    self.class.eql?(other.class) & self.name.eql?(other.name)
+  end
+  def save
+    result = DB.exec("INSERT INTO volunteers (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first().fetch("id").to_i
+  end
+  def self.clear
+  DB.exec("DELETE FROM volunteers *;")
 end
-end 
+def self.find(id)
+   volunteer= DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
+   name = volunteer.fetch("name")
+   id = volunteer.fetch("id").to_i
+   Volunteer.new({:name => name, :id => id})
+ end
+end
--- spec/volunteer_spec.rb
+++ spec/volunteer_spec.rb
@@ -5,3 +5,32 @@ describe('.all') do
     expect(Volunteer.all).to(eq([]))
   end
 end
+
+describe('#save') do
+  it("saves a volunteer") do
+    volunteer = Volunteer.new({:name => "Masha", :id => nil})
+    volunteer.save()
+    volunteer2 = Volunteer.new({:name => "Another Volunteer", :id => nil})
+    volunteer2.save()
+    expect(Volunteer.all).to(eq([volunteer, volunteer2]))
+  end
+  describe('.clear') do
+  it("clears all volunteers") do
+    volunteer = Volunteer.new({:name => "Masha", :id => nil})
+    volunteer.save()
+    volunteer2 = Volunteer.new({:name => "Another Volunteer", :id => nil})
+    volunteer2.save()
+    Volunteer.clear
+    expect(Volunteer.all).to(eq([]))
+  end
+end
+describe('.find') do
+  it("finds a volunreer by id") do
+    volunreer = Volunteer.new({:name => "Maha", :id => nil})
+    volunreer.save()
+    volunreer2 = Volunteer.new({:name => "Another Volunteer", :id => nil})
+    volunreer2.save()
+    expect(Volunteer.find(volunreer.id)).to(eq(volunreer))
+  end
+end
+end
