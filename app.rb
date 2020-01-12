6e2ca302ff51349b320d883d0dcc297571f2227d
Masha Ivy <mashanechmo@gmail.com>
2020-01-12 13:41:33 -0800

    add a few routes(to display and add projects) and views for them

--- app.rb
+++ app.rb
@@ -1,3 +1,41 @@
-require("pg")
-
+require 'sinatra'
+require 'sinatra/reloader'
+require './lib/project.rb'
+require './lib/volunteer.rb'
+require 'pg'
+require 'pry'
 DB = PG.connect({:dbname => "volunteer_tracker"})
+get('/') do
+  redirect to('/projects')
+end
+get('/projects') do
+  @projects = Project.all
+  # @projects_sold = Project].all_sold
+  erb(:projects)
+end
+get ('/projects/new') do
+  erb(:new_project)
+end
+post ('/projects') do
+  name = params[:project_name]
+  project = Project.new({:name => name, :id => nil})
+  project.save()
+  redirect to('/projects')
+end
+get ('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
+end
+post ('/new_volunteer') do
+  name = params.fetch("name")
+  project_id = params.fetch("project_id")
+  volunteer = Volunteer.new({:name => name, :project_id => project_id, :id => nil})
+  volunteer.save
+  @projects = Project.all
+  @volunteers = Volunteer.all
+  erb(:projects)
+end
+# get ('/projects/:id') do
+#   @project = Project].find(params[:id].to_i())
+#   erb(:project)
+# end
