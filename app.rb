5f349deeb64250bdc81a92e136f24934848d5dfa
Masha Ivy <mashanechmo@gmail.com>
2020-01-12 23:20:36 -0800

    add more routes to update, delete projects and add volunteers

--- app.rb
+++ app.rb
@@ -35,7 +35,34 @@ post ('/new_volunteer') do
   @volunteers = Volunteer.all
   erb(:projects)
 end
-# get ('/projects/:id') do
-#   @project = Project].find(params[:id].to_i())
-#   erb(:project)
-# end
+get ('/projects/:id/edit') do
+  @project = Project.find(params[:id].to_i())
+  erb(:edit_project)
+end
+patch ('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.update(params[:name])
+  redirect to('/projects')
+end
+post ('/projects/:id/volunteers') do
+  @project = Project.find(params[:id].to_i())
+  volunteer = Volunteer.new({:name => params[:volunteer_name], :project_id => @project.id, :id => nil})
+  volunteer.save()
+  erb(:project)
+end
+get ('/projects/:id/volunteers/:volunteer_id') do
+  @volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  erb(:volunteer)
+end
+patch ('/projects/:id/volunteers/:volunteer_id') do
+  @project = Project.find(params[:id].to_i())
+  volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  volunteer.update(params[:name], @project.id)
+  erb(:project)
+end
+delete ('/projects/:id/volunteers/:volunteer_id') do
+  volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  volunteer.delete
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
+end
